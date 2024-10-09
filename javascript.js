751490afadc08c5a124ed9276a165c3fac6c484c
Lucoria <183291283+Lucoria1@users.noreply.github.com>
2024-10-09 18:10:17 +0100

    added computer generated move

---
+++ javascript.js
@@ -0,0 +1,21 @@
+/*
+user picks an option (R/P/S)
+computer randomly generates an option
+compare the options and produce result
+display result
+*/
+
+
+function getComputerChoice () {
+    const computerChoice = Math.random();
+    if (computerChoice <= 1/3) return "Rock";
+    else if (computerChoice > 1/3 && computerChoice <= 2/3) return "Paper";
+    else if (computerChoice > 2/3) return "Scissors";
+}
+
+console.log(getComputerChoice())
+
+function getHumanChoice () {
+    const humanChoice
+}
+
